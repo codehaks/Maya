a8c5655fce364c59e10e0e4e90c2fa99832bac4c
Hakim Ghods <ghods.hakim@hotmail.com>
2021-05-18 11:45:27 +0430

    Movie

---
+++ MyApp/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyApp.Data;
+using MyApp.Models;
+
+namespace MyApp.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        public IndexModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IList<Movie> MovieList { get; set; }
+        public void OnGet()
+        {
+            MovieList = _db.Movies.ToList();
+        }
+    }
+}
