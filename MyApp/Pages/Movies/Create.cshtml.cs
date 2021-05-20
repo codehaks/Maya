ebe9df918df5b708636764ecf5f6ce955cfa936f
Hakim Ghods <ghods.hakim@hotmail.com>
2021-05-20 10:48:36 +0430

    Validations

--- MyApp/Pages/Movies/Create.cshtml.cs
+++ MyApp/Pages/Movies/Create.cshtml.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -12,10 +14,14 @@ namespace MyApp.Pages.Movies
     public class CreateModel : PageModel
     {
         [BindProperty]
+        [Required(ErrorMessage ="Name is needed!")]
+        [StringLength(maximumLength:50,MinimumLength =2,ErrorMessage = "Length must be between {1} and {2}")]
         public string Name { get; set; }
 
         [BindProperty]
-        public int Year { get; set; }
+        [Required(ErrorMessage ="Movie must have a year!")]
+        [Range(1950,2021,ErrorMessage ="Year starts with {1} to {2}")]
+        public int? Year { get; set; }
 
         private readonly AppDbContext _db;
 
@@ -26,10 +32,14 @@ namespace MyApp.Pages.Movies
 
         public IActionResult OnPost()
         {
+            if (ModelState.IsValid == false)
+            {
+                return Page();
+            }
             var movie = new Movie
             {
                 Name = Name,
-                Year = Year
+                Year = Year.Value
             };
 
             _db.Movies.Add(movie);
