40d5b32d45cf14f50885f1bbf1185ff6a73d7802
Hakim Ghods <ghods.hakim@hotmail.com>
2021-05-30 10:42:29 +0430

    Cast

--- MyApp/Models/Actor.cs
+++ MyApp/Models/Actor.cs
@@ -10,5 +10,7 @@ namespace MyApp.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public IList<Movie> Movies { get; set; }
     }
 }
--- MyApp/Models/Movie.cs
+++ MyApp/Models/Movie.cs
@@ -63,5 +63,7 @@ namespace MyApp.Models
         public int GenreId { get; set; }
         public Genre Genre { get; set; }
 
+        public IList<Actor> Cast { get; set; }
+
     }
 }
