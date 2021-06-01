3e1cfd8d42df7d71211338eb324a143f062e95a2
Hakim Ghods <ghods.hakim@hotmail.com>
2021-06-01 12:28:14 +0430

    Search

--- MyApp/Pages/Index.cshtml.cs
+++ MyApp/Pages/Index.cshtml.cs
@@ -4,16 +4,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyApp.Data;
+using MyApp.Models;
 
 namespace MyApp
 {
     public class IndexModel : PageModel
     {
-        public IActionResult OnGet()
+        private readonly AppDbContext _db;
+
+        public IndexModel(AppDbContext db)
         {
-            return Page();
+            _db = db;
         }
 
-       
+        public IList<Movie> MovieList { get; set; }
+
+
+        public string Term { get; set; }
+        public void OnGet(string term)
+        {
+            if (string.IsNullOrEmpty(term))
+            {
+                MovieList = _db.Movies.ToList();
+            }
+            else
+            {
+                MovieList = _db.Movies.Where(m => m.Name.Contains(term)).ToList();
+            }
+
+            Term = term;
+
+
+        }
     }
 }
