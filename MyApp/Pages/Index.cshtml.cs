9291a59472bbf8f14fc0c50c6f37a09cd1ae1dc9
Hakim Ghods <ghods.hakim@hotmail.com>
2021-06-25 17:01:45 +0430

    Query

--- MyApp/Pages/Index.cshtml.cs
+++ MyApp/Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MyApp.Data;
 using MyApp.Models;
@@ -20,19 +21,41 @@ namespace MyApp
         }
 
         public IList<Movie> MovieList { get; set; }
-
-
         public string Term { get; set; }
-        public void OnGet(string term)
+        public int Score { get; set; }
+
+        public SelectList GenreList { get; set; }
+
+        public void OnGet(string term, int score, int genreId)
         {
+
+            var genres = _db.Genres.ToList();
+            GenreList = new SelectList(genres, "Id", "Name");
+
+            IQueryable<Movie> movieQuery;
+
             if (string.IsNullOrEmpty(term))
             {
-                MovieList = _db.Movies.ToList();
+                movieQuery = _db.Movies.Include(m => m.Genre);
             }
             else
             {
-                MovieList = _db.Movies.Where(m => m.Name.Contains(term)).ToList();
+                movieQuery = _db.Movies.Include(m => m.Genre).Where(m => m.Name.Contains(term));
             }
+
+            if (score > 0)
+            {
+                movieQuery = movieQuery.Where(m => m.Score != null && (int)m.Score >= score);
+            }
+
+            if (genreId > 0)
+            {
+                movieQuery = movieQuery.Where(m => m.Genre.Id == genreId);
+            }
+
+
+
+            MovieList = movieQuery.ToList();
 
             Term = term;
 
